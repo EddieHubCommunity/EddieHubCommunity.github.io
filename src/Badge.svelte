b12c41a39baf5bb4b6edddbff91e1fcbdde4dbbd
Mohamed Al-Rifay <mohamed.abdelattif@fci.helwan.edu.eg>
2020-10-02 06:00:20 +0200

    feat(mods): refactor moderators card #82

---
+++ src/Badge.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { badges } from "./data";
+  export let type;
+  const { title, classes } = badges[type];
+</script>
+
+<button class="rounded px-1 mr-1 text-sm ml-auto {classes}">{title}</button>
