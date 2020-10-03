9e899a2eabcf1b69bf621edfedfd67f24ea9d94b
RemcoHalman <remco.halman@gmail.com>
2020-10-03 16:22:16 +0200

    Restructure folder to show Vue as main App

--- cypress/support/step_definitions/navigate.js
+++ cypress/support/step_definitions/navigate.js
@@ -1,6 +1,6 @@
 import { Given } from "cypress-cucumber-preprocessor/steps";
 
-const url = "http://127.0.0.1:8080/";
+const url = "http://localhost:8080/";
 Given("I open the website", () => {
   cy.visit(url);
 });
--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
-import App from './App.svelte';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-const app = new App({
-	target: document.body
-});
-
-export default app;
+createApp(App).mount('#app')
