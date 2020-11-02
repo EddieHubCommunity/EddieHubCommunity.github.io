4be724f061f1bf98c5f71030b2792f565ece33a2
vivek80801 <vivek80801@gmail.com>
2020-11-02 15:29:11 +0530

    tests are working now
    cypress > support > step_definitions > navigate.js
    
    In this file url is changed to 'http://localhost:3000'

--- cypress/integration/homepage/homepage.spec.js
+++ cypress/integration/homepage/homepage.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+
 Given("I open the website", ()=>{
     cy.visit('http://localhost:3000')
 })
--- cypress/support/step_definitions/navigate.js
+++ cypress/support/step_definitions/navigate.js
@@ -1,6 +1,6 @@
 import { Given } from 'cypress-cucumber-preprocessor/steps';
 
-const url = 'http://127.0.0.1:8080/';
+const url = 'http://localhost:3000/';
 Given('I open the website', () => {
   // eslint-disable-next-line no-undef
   cy.visit(url);
