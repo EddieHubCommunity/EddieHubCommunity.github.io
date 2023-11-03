101b451957b711b701cb037f6cb8690d03069b01
Amanda <97615019+amandamartin-dev@users.noreply.github.com>
2023-11-03 08:19:35 -0400

    Update Faqs.jsx
    remove background image from template

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 
 import { Container } from '@/components/Container'
-import backgroundImage from '@/images/background-faqs.jpg'
 
 const faqs = [
   [
@@ -64,14 +63,6 @@ export function Faqs() {
       aria-labelledby="faq-title"
       className="relative overflow-hidden bg-slate-50 py-20 sm:py-32"
     >
-      <Image
-        className="absolute left-1/2 top-0 max-w-none -translate-y-1/4 translate-x-[-30%]"
-        src={backgroundImage}
-        alt=""
-        width={1558}
-        height={946}
-        unoptimized
-      />
       <Container className="relative">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2
