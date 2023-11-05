138560172fec4429a4764f4f5d43c5380664918d
Eddie Jaoude <eddie@jaoudestudios.com>
2023-11-05 09:56:55 +0000

    fix: logo link to root

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -98,11 +98,11 @@ export function Header() {
       <Container>
         <nav className="relative z-50 flex justify-between">
           <div className="flex items-center md:gap-x-12">
-            <Link href="#" aria-label="Home">
+            <Link href="/" aria-label="Home">
               <Logo className="h-10 w-auto" />
             </Link>
             <div className="hidden md:flex md:gap-x-6">
-            <NavLink href="/about">About</NavLink>
+              <NavLink href="/about">About</NavLink>
               <NavLink href="/#projects">Projects</NavLink>
               <NavLink href="/#testimonials">Testimonials</NavLink>
               <NavLink href="/#moderators">Moderators</NavLink>
