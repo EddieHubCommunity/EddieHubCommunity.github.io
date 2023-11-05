dd997aa3ddf56b199123ef67ad20bfe86e468164
Eddie Jaoude <eddie@jaoudestudios.com>
2023-11-05 09:51:21 +0000

    Update src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -103,7 +103,7 @@ export function Header() {
             </Link>
             <div className="hidden md:flex md:gap-x-6">
             <NavLink href="/about">About</NavLink>
-              <NavLink href="/#features">Features</NavLink>
+              <NavLink href="#projects">Projects</NavLink>
               <NavLink href="/#testimonials">Testimonials</NavLink>
               <NavLink href="/#moderators">Moderators</NavLink>
             </div>
