c4d641fc10d91462d00f5e16bc1d0e65073f9e5d
Abdulquddus Abdulrahman (Lexzee) <66857875+lexzee@users.noreply.github.com>
2023-11-06 11:16:01 +0100

    Update PrimaryFeatures.jsx

--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -129,7 +129,7 @@ export function PrimaryFeatures() {
                       <Image
                         className="w-full"
                         src={feature.image}
-                        alt={feature.title}
+                        alt={`image of ${feature.title}`}
                         priority
                         sizes="(min-width: 1024px) 67.8125rem, (min-width: 640px) 100vw, 45rem"
                       />
