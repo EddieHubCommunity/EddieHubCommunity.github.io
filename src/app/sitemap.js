a133e22358e336dc4699621b69616cf0295b8a70
Suyog Kurlekar <58219746+ctrl07@users.noreply.github.com>
2023-12-24 19:29:22 +0530

    feat: add sitemap (#329)
    Co-authored-by: Krish Gupta <xkrishguptaa@gmail.com>

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://eddiehub.org',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ]
+}
